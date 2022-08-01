e9b8752a36cc1d9053b0d84837a928eaf5e2d53e
Renatoogata <renatoogata95@gmail.com>
2022-08-01 12:37:35 -0300

    testes statement

--- src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -2,7 +2,7 @@ import { hash } from "bcryptjs";
 import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
 import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
 import { CreateStatementUseCase } from "./CreateStatementUseCase"
-
+import { OperationType } from "../../entities/Statement";
 
 let inMemoryUserRepository: InMemoryUsersRepository;
 let inMemoryStatementRepository: InMemoryStatementsRepository;
@@ -24,7 +24,32 @@ describe("Create an Statement deposit/withdwar", () => {
       password: passWordHash,
     };
 
-    await inMemoryUserRepository.create(user);
+
+    const response = await inMemoryUserRepository.create(user);
+
+    if (response.id) {
+
+      const deposit = await inMemoryStatementRepository.create({
+        type: OperationType.DEPOSIT,
+        amount: 1000,
+        description: "teste",
+        user_id: response.id,
+      });
+
+      const withdraw = await inMemoryStatementRepository.create({
+        type: OperationType.WITHDRAW,
+        amount: 1000,
+        description: "teste",
+        user_id: response.id,
+      });
+
+      expect(deposit).toHaveProperty("id");
+      expect(deposit).toHaveProperty("type", "deposit");
+
+      expect(withdraw).toHaveProperty("id");
+      expect(withdraw).toHaveProperty("type", "withdraw");
+
+    }
 
   })
 
